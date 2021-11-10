016487a54a126000d183e7c3ad9f44919c2b7268
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2021-11-11 00:30:37 +0600

    modifies the sobel
    modifies the sobel

--- classwork_1.py
+++ classwork_1.py
@@ -4,58 +4,56 @@ import numpy as np
 
 SIZE=250
 
-def normalize(img):
-    nImg = np.zeros(img.shape)
+def image_normalize(img):
+    output_image = np.zeros(img.shape)
     
-    max_ = img.max()
-    min_ = img.min()
+    max_img = img.max()
+    min_img = img.min()
     
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            nImg[i][j] = (img[i][j]-min_)/(max_-min_) * 255
+            output_image[i][j] = (img[i][j]-min_img)/(max_img-min_img) * 255
     
-    return nImg
+    return output_image
 
-#img = cv2.imread("1st.png", cv2.IMREAD_GRAYSCALE)
+
 img = cv2.imread("Lena.jpg", cv2.IMREAD_GRAYSCALE)
 img = cv2.resize(img, (SIZE, SIZE))
 cv2.imshow("input", img)
 cv2.waitKey(0)
 
-#cv2.destroyAllWindows()
 
 sobel_x = np.array([[-1, 0, 1],
-           [-2, 0, 2],
-           [-1, 0, 1]])
+                    [-2, 0, 2],
+                    [-1, 0, 1]])
 
 sobel_y = np.array([[1, 2, 1],
-           [0, 0, 0],
-           [-1, -2, -1]])
+                    [0, 0, 0],
+                    [-1,-2,-1]])
 
 sx = cv2.filter2D(src=img, ddepth=-1, kernel=sobel_x)
-#sx = normalize(sx)
 
 cv2.imshow("sobel_x", sx)
 cv2.waitKey(0)
 
 sy = cv2.filter2D(src=img, ddepth=-1, kernel=sobel_y)
-#sy = normalize(sy)
+
 
 cv2.imshow("sobel_y", sy)
 cv2.waitKey(0)
 
-tr = 50
+threshold = 50
 
-sx = normalize(sx)
-sy = normalize(sy)
+sx = image_normalize(sx)
+sy = image_normalize(sy)
 
-out = np.sqrt(np.multiply(sx, sx) + np.multiply(sy, sy))
-out = normalize(out)
-#out = np.array(out, dtype='uint8')
-out[out>=tr] = 255
-out[out<tr] = 0
+output_image = np.sqrt(np.multiply(sx, sx) + np.multiply(sy, sy))
+output_image = image_normalize(output_image)
 
-cv2.imshow("output", out)
+output_image[output_image >=threshold] = 255
+output_image[output_image <threshold] = 0
+
+cv2.imshow("output", output_image)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
