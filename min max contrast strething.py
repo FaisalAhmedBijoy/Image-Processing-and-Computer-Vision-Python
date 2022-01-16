0dc865423f5ae31ec0835d4b7a6dff186d3f0154
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2022-01-16 23:40:00 +0600

    Update min max contrast strething.py

--- min max contrast strething.py
+++ min max contrast strething.py
@@ -9,7 +9,7 @@ minmax_img=np.zeros((img.shape[0],img.shape[1]),dtype='uint8')
 for i in range(img.shape[0]):
     for j in range(img.shape[1]):
         xinput=img.item(i,j)
-        minmax_img[i,j]=255*(img[i,j]*np.min(img)) / (np.max(img)-np.min(img))  
+        minmax_img[i,j]=255*(img[i,j]-np.min(img)) / (np.max(img)-np.min(img))  
 
 cv2.imshow('min max',minmax_img)
 cv2.waitKey(0) 
